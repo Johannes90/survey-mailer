d27f91d48dded7b529e04ec2bb1fd4cee65f5f38
Johannes Herrmann <johannes.herrmann2012@gmail.com>
2018-02-02 15:53:31 +0100

    before switch to react-scripts-css-modules

---
+++ server/client/src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const SurveyNew = () => <h1>SurveyNew</h1>;
+const Landing = () => <h1>Landing</h1>;
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/surveys" exact component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
